f082208fa426685dc6fd1f282fb53601f06c50fb
Tanzia Islam <tanzich2018@gmail.com>
2022-04-28 15:06:25 -0700

    final work for dashboard

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS, cross_origin
 from transformers import pipeline
+import nltk
+import sys
 import json
 import requests
 
@@ -19,7 +21,6 @@ headers = {"Authorization": f"Bearer {API_TOKEN}", "Access-Control-Allow-Origin"
 @app.route('/sentiment', methods=['GET','OPTIONS','POST'])
 def sentiment():
     data = request.get_data()
-    print(data)
     response = requests.request("POST", API_URL, headers=headers, data=data)
     return jsonify(json.loads(response.content.decode("utf-8")))
 
@@ -31,11 +32,31 @@ headers_emotion = {"Authorization": f"Bearer {API_TOKEN}", "Access-Control-Allow
 @app.route('/emotion', methods=['OPTIONS','POST'])
 def emotion():
     data_emo = request.get_data()
-    print(data_emo)
     response_emo = requests.request("POST", API_URL_Emotion, headers=headers_emotion, data=data_emo)
     return jsonify(json.loads(response_emo.content.decode("utf-8")))
 
 
+API_URL_Stars = "https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment"
+headers_emotion = {"Authorization": f"Bearer {API_TOKEN}", "Access-Control-Allow-Origin" : API_URL_Emotion}
+
+@app.route('/star', methods=['OPTIONS','POST'])
+def star():
+    data_emo = request.get_data()
+    response_emo = requests.request("POST", API_URL_Stars, headers=headers_emotion, data=data_emo)
+    return jsonify(json.loads(response_emo.content.decode("utf-8")))
+
+
+@app.route('/dataCleaning', methods=['OPTIONS','POST'])
+def dataCleaning():
+    data = request.get_data()
+    word_list = []
+    nltk.download('words')
+    words = set(nltk.corpus.words.words())
+    cleaned_input = (w for w in nltk.wordpunct_tokenize(data.decode('utf-8')) if w.lower() in words)
+    for w in nltk.wordpunct_tokenize(data.decode('utf-8')):
+      if w.lower() in words:
+        word_list.append(w)
+    return " ".join(word_list)
 
 if __name__ == '__main__':
   app.run(debug=True)
